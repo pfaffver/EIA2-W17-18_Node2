66a05734c9bfd30d78ed9c019d2a1c0c552b9a96
Verena <Verena@LAPTOP-T6V1EL0O.eduroam.hs-furtwangen.de>
2018-01-16 16:57:09 +0100

    11

--- ServerTest.ts
+++ ServerTest.ts
@@ -37,7 +37,7 @@ namespace ServerTest {
         _response.setHeader("Access-Control-Allow-Origin", "*");
         
         // Response-Body
-        _response.write("Ich höre Stimmen!!<br>");
+        _response.write("Dein Warenkorb<br>");
         _response.write("Port: " + port + "<br>");
         _response.write("Method: " + _request.method + "<br>");
         _response.write("Url: " + _request.url + "<br>");
@@ -51,5 +51,6 @@ namespace ServerTest {
         
         // Antwort abschließen und abschicken
         _response.end();
+        
     }
 }

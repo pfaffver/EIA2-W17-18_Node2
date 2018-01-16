e1374d00ad2d4205f793e334b22d0293e6163544
Verena <Verena@LAPTOP-T6V1EL0O>
2018-01-16 19:39:56 +0100

    11

--- ServerTest.js
+++ ServerTest.js
@@ -29,7 +29,7 @@ var ServerTest;
         // Header: ?
         _response.setHeader("Access-Control-Allow-Origin", "*");
         // Response-Body
-        _response.write("<span id='WK'>Warenkorb</span>" + "<br>");
+        _response.write("<p>Warenkorb</p>" + "<br>");
         // _response.write("Port: " + port + "<br>");
         //  _response.write("Method: " + _request.method + "<br>");
         // _response.write("Url: " + _request.url + "<br>");

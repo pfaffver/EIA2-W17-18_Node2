91bd677ba0bf15e8449fe5b240ec75fb8adff57c
Verena <Verena@LAPTOP-T6V1EL0O>
2018-01-20 17:20:01 +0100

    11

--- ServerTest.js
+++ ServerTest.js
@@ -78,8 +78,6 @@ var ServerTest;
         _response.write("Wohnort" + ": " + query["Ort"] + "<br>");
         _response.write("Postleitzahl" + ": " + query["PLZ"] + "<br>");
         _response.write("E-Mail" + ": " + query["E-Mail"] + "<br>");
-        for (let key in query)
-            _response.write(key + ": " + query[key]);
         // Antwort abschlie�en und abschicken
         _response.end();
     }
